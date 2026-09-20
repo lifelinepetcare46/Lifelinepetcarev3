8696eb3cf849242d683e4f700e63519e6c4d3e7e
lifelinepetcare46 <lifelinepetcare46@gmail.com>
2026-09-20 00:48:37 -0700

    Update sitemap domain to lifelinepetcare.in

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,5 @@
 export default async function sitemap() {
-  const baseUrl = "https://lifeline-pet-care-v3.vercel.app";
+  const baseUrl = "https://lifelinepetcare.in";
 
   const routes = [
     "",
